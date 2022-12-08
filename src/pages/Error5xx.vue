58174e9255fe1d45281e22ef262da2f7f5f6f7f0
MisterLobo <algae1234567890@gmail.com>
2022-12-08 14:40:36 +0800

    initial commit

---
+++ src/pages/Error5xx.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        Internal Server Error
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Oops, there was an error on the server.
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
